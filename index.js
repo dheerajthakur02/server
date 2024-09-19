ed5901731e5953ee1f18b41ceeb22f0533dd747c
Dheeraj Kumar Thalur <114847997+dheerajthakur02@users.noreply.github.com>
2024-09-19 13:09:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -22,31 +22,30 @@ const userSchema = new mongoose.Schema({
 
 const User = mongoose.model("User", userSchema);
 
-// Middleware
+
 app.use(bodyParser.json());
 app.use(cors());
 app.use(express.json());
 
-// Basic Route
+
 app.get("/", (req, res) => {
   res.send("<h1>Server is working</h1>");
 });
 
-// Add or Update User Route
+
 app.post("/add", async (req, res) => {
   const { name, userHandle, imageUrls } = req.body;
 
   try {
-    // Check if the user already exists by userHandle
+
     let user = await User.findOne({ userHandle });
 
     if (user) {
-      // User exists, update the existing user's image URLs
-      user.imageUrls = [...user.imageUrls, ...imageUrls]; // Append new image URLs to the existing ones
+    
+      user.imageUrls = [...user.imageUrls, ...imageUrls]; 
       await user.save();
       res.status(200).send({ message: "User updated successfully" });
     } else {
-      // User does not exist, create a new user
       user = new User({ name, userHandle, imageUrls });
       await user.save();
       res.status(201).send({ message: "Successfully stored in database" });
@@ -56,7 +55,6 @@ app.post("/add", async (req, res) => {
   }
 });
 
-// Get Users Route
 app.get("/users", async (req, res) => {
   try {
     const users = await User.find({});
@@ -66,7 +64,4 @@ app.get("/users", async (req, res) => {
   }
 });
 
-// Start the Server
-app.listen(5000, () => {
-  console.log("Server is running on port: 5000");
-});
+export default app;
